a4c0c775d10d0b73b83283c62cedf3a06e34bc51
GabyMarr <gabriel.marrero9@upr.edu>
2024-08-02 13:14:38 -0400

    Up and Running

--- TradingBot.py
+++ TradingBot.py
@@ -5,7 +5,7 @@ from lumibot.traders import Trader
 from datetime import datetime
 from alpaca_trade_api import REST
 from timedelta import Timedelta
-
+from finbert_utils import estimate_sentiment
 
 # ApiKey, ApiSeceret, and baseURL is unique to everyone.
 # 1. Make an account on " https://alpaca.markets/ "
@@ -16,62 +16,90 @@ from timedelta import Timedelta
 # 6. Secret is your API Secret #
 
 
-API_KEY = "PK0F70UN2DTAQ3GDV100"
-API_SECRET = "9Ze3Ll6p65fgZmzoqf3e8bsARzsRAKV9Xpu3tU6n"
+# API_KEY = "PK0F70UN2DTAQ3GDV100"
+# API_SECRET = "9Ze3Ll6p65fgZmzoqf3e8bsARzsRAKV9Xpu3tU6n"
+# BASE_URL = "https://paper-api.alpaca.markets/v2"
+
+API_KEY = "PKS2U3A28GLKDSB5GZFN"
+API_SECRET = "1dUoUIUmTXDSq4gNm7hBnHMWjJDwkZedJifYraX2"
 BASE_URL = "https://paper-api.alpaca.markets/v2"
 
-AplacaCreds = { "API_KEY": API_KEY, "API_SECRET": API_SECRET, "PAPER": True }
+ALPACA_CREDS = { 
+    "API_KEY": API_KEY, 
+    "API_SECRET": API_SECRET,
+    "PAPER": True 
+    }
 
-class machineLearningTrader(Strategy):
-    def bootUpBot(self, symbol:str = "SPY", cashAtRisk:float = 0.5): #Starts up the bot
+
+class MLTrader(Strategy):
+    def initialize(self, symbol:str = "SPY", cash_at_risk:float = 0.5): #Starts up the bot
         self.symbol = symbol
         self.sleeptime = "24H"
         self.last_trade = None
+        self.cash_at_risk = cash_at_risk #
         self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
         pass #Setup 
 
     def position_sizing(self):
-        buyingPower = self.get_cash() 
-        lastPrice = self.get_last_price(self.symbol)
-        quantity = round(buyingPower * self.cashAtRisk / lastPrice, 0)
-        return buyingPower, lastPrice, quantity
+        cash = self.get_cash() 
+        last_price = self.get_last_price(self.symbol)
+        quantity = round(cash * self.cash_at_risk / last_price, 0)
+        return cash, last_price, quantity
     
     def get_dates(self):
         today = self.get_datetime()
         threeDaysPrior = today - Timedelta(days=3)
-        return today.strfttime("%Y-%m-%d"), threeDaysPrior.strfttime("%Y-%m-%d")
+        return today.strftime('%Y-%m-%d'), threeDaysPrior.strftime('%Y-%m-%d')
     
-    def get_news(self):
+    def get_sentiment(self):
         today, threeDaysPrior = self.get_dates()
         news = self.api.get_news(symbol=self.symbol, start= threeDaysPrior, end=today)
         news = [ev.__dict__["_raw"]["headline"] for ev in news]
-        return news
+        probability, sentiment = estimate_sentiment(news)
+        return probability, sentiment
 
     
 
-    def refreshBot(self): #runs everytime we get new information
+    def on_trading_iteration(self): #runs everytime we get new information
         # Trading Logic 
-        buyingPower, lastPrice, quantity = self.position_sizing()
-        if buyingPower > lastPrice: # checks that we have cash to buy shares 
-            if self.last_trade == None:
-                news = self.get_news()
+        
+        cash, last_price, quantity = self.position_sizing()
+        probability, sentiment = self.get_sentiment()
+        if cash > last_price:
+            if sentiment == "positive" and probability > .999: # checks that we have cash to buy shares 
+                if self.last_trade == "sell":
+                    self.sell_all()
+                print(probability, sentiment)
+
                 order = self.create_order(
                     self.symbol,
                     quantity, 
                     "buy",
                     type="bracket",
-                    takeProfit = lastPrice*1.20, # sells if hits 20% profit on trade
-                    stopLoss = lastPrice*0.95 # sells if loss is -5% on trade
+                    take_profit_price = last_price*1.20, # sells if hits 20% profit on trade
+                    stop_loss_price = last_price*0.95 # sells if loss is -5% on trade
                     )
                 self.submit_order(order)
                 self.last_trade = "buy"
+            elif sentiment == 'negative' and probability > .999:
+                if self.last_trade == "buy":
+                    self.sell_all()
+                order = self.create_order(
+                    self.symbol, 
+                    quantity, 
+                    "sell",
+                    type="bracket", 
+                    take_profit_price= last_price*.8, 
+                    stop_loss_price = last_price*1.05
+                )
+                self.submit_order(order)
+                self.last_trade = "sell"
 
-
-startDate = datetime(2024, 5, 15) #Y/M/D
+# df.index = df.index.tz_convert('utc')
+startDate = datetime(2023, 5, 20) #Y/M/D
 endDate = datetime(2024, 5, 31) #Y/M/D
 
-broker = Alpaca(AplacaCreds)
-strategy = machineLearningTrader(name = "mlStrat", broker = broker, parameters = {"symbol":"SPY", "cashAtRisk": 0.5})
+broker = Alpaca(ALPACA_CREDS)
+strategy = MLTrader(name = "mlStrat", broker = broker, parameters = {"symbol":"SPY", "cash_at_risk": 0.5})
 
-strategy.backtest(YahooDataBacktesting, startDate, endDate, parameters={"symbol":"SPY", "cashAtRisk": 0.5})
-
+strategy.backtest(YahooDataBacktesting, startDate, endDate, parameters={"symbol":"SPY", "cash_at_risk": 0.5})
